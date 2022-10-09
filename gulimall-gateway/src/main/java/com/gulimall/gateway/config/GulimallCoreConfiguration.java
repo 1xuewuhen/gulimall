3c3c4e1724b02873169f7acb83abeb4945ba24ef
xuehuhen <1984451361@qq.com>
2022-10-09 10:15:53 +0800

    解决各个模块之间跨域问题

---
+++ gulimall-gateway/src/main/java/com/gulimall/gateway/config/GulimallCoreConfiguration.java
@@ -0,0 +1,25 @@
+package com.gulimall.gateway.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GulimallCoreConfiguration {
+
+    @Bean
+    public CorsWebFilter corsWebFilter(){
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedOriginPattern("*");
+//        configuration.addAllowedOrigin("*");allowedOriginPatterns
+        configuration.setAllowCredentials(true);
+        source.registerCorsConfiguration("/**",configuration);
+        return new CorsWebFilter(source);
+    }
+}
