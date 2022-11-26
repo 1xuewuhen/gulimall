89d53a62fe65b480132a0dfbd10c003d6739ff43
xuehuhen <1984451361@qq.com>
2022-11-26 12:54:27 +0800

    项目集成第三方工具OSS云存储

--- gulimall-product/src/test/java/com/xwh/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/xwh/gulimall/product/GulimallProductApplicationTests.java
@@ -1,23 +1,26 @@
 package com.xwh.gulimall.product;
 
-import com.xwh.gulimall.product.entity.BrandEntity;
 import com.xwh.gulimall.product.service.BrandService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+
 @SpringBootTest
 class GulimallProductApplicationTests {
 
-
-    @Autowired
-    private BrandService brandService;
-    @Test
-    void contextLoads() {
-        BrandEntity brandEntity = new BrandEntity();
-        brandEntity.setName("华为");
-        brandService.save(brandEntity);
-        System.out.println("success");
-    }
+    /**
+     * 用户登录名称 cfy-xwh@1562923757844605.onaliyun.com
+     * AccessKey ID LTAI5tPNR9fA2Tu1vk8zAPGf
+     * AccessKey Secret wAMKLtrXDBBpntB5CkFVIu6Giz5QXe
+     *
+     *
+     * 用户登录名称 gulimall@1562923757844605.onaliyun.com
+     * AccessKey ID LTAI5tNQ9u58Jh4RWv2Menk9
+     * AccessKey Secret n7DaMqLlGdb72SrhnG9QJrvFRhLsIO
+     */
 
 }
