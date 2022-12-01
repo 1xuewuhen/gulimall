c6f3127464a156bb489861a6f93848381635daa2
xuehuhen <1984451361@qq.com>
2022-12-01 21:18:07 +0800

    项目集成es

---
+++ gulimall-search/src/main/java/com/xwh/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -0,0 +1,23 @@
+package com.xwh.gulimall.search.config;
+
+
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GulimallElasticSearchConfig {
+
+    @Bean
+    public RestHighLevelClient esRestClient(){
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("127.0.0.1",9200,"http")
+                )
+        );
+        return client;
+    }
+}
