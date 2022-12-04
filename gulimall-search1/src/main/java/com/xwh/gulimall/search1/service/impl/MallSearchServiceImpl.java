8a6617794bb991cc1ac2c88f6d93f6f0161c4c5d
xuehuhen <1984451361@qq.com>
2022-12-04 22:09:44 +0800

    项目进行检索服务查询条件构建完成

--- gulimall-search1/src/main/java/com/xwh/gulimall/search1/service/impl/MallSearchServiceImpl.java
+++ gulimall-search1/src/main/java/com/xwh/gulimall/search1/service/impl/MallSearchServiceImpl.java
@@ -1,15 +1,128 @@
 package com.xwh.gulimall.search1.service.impl;
 
+import com.xwh.gulimall.search1.config.GulimallElasticSearchConfig;
+import com.xwh.gulimall.search1.constant.EsConstant;
 import com.xwh.gulimall.search1.service.MallSearchService;
 import com.xwh.gulimall.search1.vo.SearchParam;
 import com.xwh.gulimall.search1.vo.SearchResult;
+import org.apache.lucene.search.join.ScoreMode;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.RestHighLevelClientBuilder;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.NestedQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
 
 
 @Service
 public class MallSearchServiceImpl implements MallSearchService {
+
+    @Autowired
+    private RestHighLevelClient client;
+
     @Override
     public SearchResult search(SearchParam param) {
+
+        // 准备检索请求
+        SearchResult result = null;
+        SearchRequest searchRequest = buildSearchRequest(param);
+        try {
+            //执行检索请求
+            SearchResponse response = client.search(searchRequest, GulimallElasticSearchConfig.COMMON_OPTIONS);
+
+            // 分析响应数据封装成我们需要的格式
+
+            result = buildSearchResult(response);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 构建结果数据
+     *
+     * @param response
+     * @return
+     */
+    private SearchResult buildSearchResult(SearchResponse response) {
+
+
         return null;
+    }
+
+
+    /**
+     * 准备检索请求
+     *
+     * @return
+     */
+    private SearchRequest buildSearchRequest(SearchParam param) {
+        //构建DSL语句
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        /**
+         * 查询 模糊匹配，过滤（按照属性，分类，品牌，价格区间，库存）
+         */
+        // 构建bool - query
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+        if (!StringUtils.isEmpty(param.getKeyword())) {
+            boolQuery.must(QueryBuilders.matchQuery("skuTitle", param.getKeyword()));
+        }
+        if (param.getCatalog3Id() != null) {
+            boolQuery.filter(QueryBuilders.termQuery("catalogId", param.getCatalog3Id()));
+        }
+        if (param.getBrandId() != null && param.getBrandId().size() > 0) {
+            boolQuery.filter(QueryBuilders.termsQuery("brandId", param.getBrandId()));
+        }
+        boolQuery.filter(QueryBuilders.termQuery("hasStock", param.getHasStock() == 1));
+        if (!StringUtils.isEmpty(param.getSkuPrice())) {
+            RangeQueryBuilder rangeQuery = QueryBuilders.rangeQuery("skuPrice");
+            String[] s = param.getSkuPrice().split("_");
+            if (s.length == 2) {
+                rangeQuery.gte(s[0])
+                        .lte(s[1]);
+            } else if (s.length == 1) {
+                if (param.getSkuPrice().startsWith("_")) {
+                    rangeQuery.lte(s[0]);
+                }
+                if (param.getSkuPrice().endsWith("_")) {
+                    rangeQuery.gte(s[0]);
+                }
+            }
+            boolQuery.filter(rangeQuery);
+        }
+        if (param.getAttrs() != null && param.getAttrs().size() > 0) {
+            for (String attrStr : param.getAttrs()) {
+                BoolQueryBuilder nestedBoolQuery = QueryBuilders.boolQuery();
+                String[] s = attrStr.split("_");
+                String attrId = s[0];
+                String[] attrValue = s[1].split(":");
+                nestedBoolQuery.must(QueryBuilders.termQuery("attrs.attrId", attrId));
+                nestedBoolQuery.must(QueryBuilders.termsQuery("attrs.attrValue", attrValue));
+                NestedQueryBuilder nestedQuery = QueryBuilders.nestedQuery("attrs", nestedBoolQuery, ScoreMode.None);
+                boolQuery.filter(nestedQuery);
+            }
+        }
+        searchSourceBuilder.query(boolQuery);
+
+        /**
+         * 排序，分页，高亮
+         */
+
+        /**
+         * 聚合分析
+         */
+
+        SearchRequest searchRequest = new SearchRequest(new String[]{EsConstant.PRODUCT_INDEX}, searchSourceBuilder);
+        return searchRequest;
     }
 }
--- gulimall-search1/src/main/java/com/xwh/gulimall/search1/vo/SearchParam.java
+++ gulimall-search1/src/main/java/com/xwh/gulimall/search1/vo/SearchParam.java
@@ -14,7 +14,7 @@ public class SearchParam {
 
     private String sort;
 
-    private Integer hasStock;
+    private Integer hasStock = 1;
 
     private String skuPrice;
 
