f5302d1f2c76be85789e555a38831b98fcfb0206
xuehuhen <1984451361@qq.com>
2023-02-26 19:08:50 +0800

    添加自定义支付功能

--- gulimall-order/src/main/java/com/xwh/gulimall/order/web/PayWebController.java
+++ gulimall-order/src/main/java/com/xwh/gulimall/order/web/PayWebController.java
@@ -1,7 +1,7 @@
 package com.xwh.gulimall.order.web;
 
 
-import com.alipay.api.AlipayApiException;
+import com.xwh.common.utils.HttpUtils;
 import com.xwh.gulimall.order.config.AlipayTemplate;
 import com.xwh.gulimall.order.service.OrderService;
 import com.xwh.gulimall.order.vo.PayVo;
@@ -10,6 +10,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
 
 @Controller
 public class PayWebController {
@@ -21,11 +23,17 @@ public class PayWebController {
     private OrderService orderService;
 
     @ResponseBody
-    @GetMapping("/payOrder")
-    public String payOrder(@RequestParam("orderSn") String orderSn) throws AlipayApiException {
-
+    @GetMapping(value = "/payOrder", produces = {"text/html"})
+    public String payOrder(@RequestParam("orderSn") String orderSn) throws Exception {
         PayVo payVo = orderService.getOrderPay(orderSn);
-        alipayTemplate.pay(payVo);
-        return "xxxx";
+        //        System.out.println(pay);
+        HashMap<String, String> map = new HashMap<>();
+        map.put("body", payVo.getBody());
+        map.put("subject", payVo.getSubject());
+        map.put("outTradeNo", payVo.getOut_trade_no());
+        map.put("totalAmount", payVo.getTotal_amount());
+        HashMap<String, String> hashMap = new HashMap<>();
+        HttpUtils.doGet("http://127.0.0.1:50010", "/pay/myZhiFuBao", "get", hashMap, map);
+        return alipayTemplate.pay(payVo);
     }
 }
